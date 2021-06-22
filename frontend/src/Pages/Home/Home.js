1965a4b76e35dc549f7e88d211b06155d2bdae72
Andrew Miller <jazznerd206@icloud.com>
2021-06-22 15:03:10 -0700

    structure

---
+++ frontend/src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles.css';
+import './styles.css';
+
+function Home() {
+    return (
+        <div className="page-wrapper">
+            <h1>HOME</h1>
+        </div>
+    )
+}
+
+export default Home
---
+++ frontend/src/Pages/Work/Work.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+function Work() {
+    return (
+        <div>
+            Work
+        </div>
+    )
+}
+
+export default Work
